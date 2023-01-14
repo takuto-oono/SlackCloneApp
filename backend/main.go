2b3f1f44b9a9556da1d5cf55cd00f45717c7ca92
TO053037 <67772806+TO053037@users.noreply.github.com>
2023-01-14 12:51:41 +0900

    add: running Golang gin

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,17 @@
-package main
+package main 
 
 import (
-	"fmt"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello Golang")
+    router := gin.Default()
+
+    router.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "Hello World",
+        })
+    })
+
+    router.Run(":8000")
 }
