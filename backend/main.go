08dbcbcdf3c5e68c9a17f7bca3aaa9c7478a4d97
TO053037 <67772806+TO053037@users.noreply.github.com>
2023-01-15 13:32:06 +0900

    add: api_1 backend

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,26 @@
-package main 
+package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 
-    router.GET("/", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "Hello World",
-        })
-    })
+	router.GET("/:x", func(c *gin.Context) {
+		x, err := strconv.Atoi(c.Param("x"))
+		if err != nil {
+			c.JSON(400, gin.H{
+				"message": "error convert int",
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"x": x + 1,
+			})
+		}
+	})
 
-    router.Run(":8000")
+	router.Run(":8000")
 }
