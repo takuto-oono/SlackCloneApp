176738bdd57120c37db6671e16f69493508dfa35
TO053037 <67772806+TO053037@users.noreply.github.com>
2023-01-18 13:15:47 +0900

    add: debug cors

--- frontend/pages/api/test_api_1.tsx
+++ frontend/pages/api/test_api_1.tsx
@@ -1,11 +1,21 @@
-const url = 'http://localhost:8000/1'
+const url = "http://localhost:8000/test/8";
 
-export default async function TestAPI1() {
-    const res = await fetch(url, {
-        method: 'GET',
-        
-    });
-    
-    const body = await res.json()
-    console.log(body)
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function testAPI1(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const data = await fetch(url, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+    console.log(data)
+    const word = await data.json()
+    console.log(word)
+    res.status(200).json({
+        message: word,
+    })
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,21 @@
-import TestAPI1 from "./api/test_api_1";
+// import TestAPI1 from "./api/test_api_1";
 
 export default function Home() {
-  console.log(TestAPI1())
+  async function TestAPI1() {
+    const url = 'http://localhost:8000/test?'
+    const res = await fetch('http://localhost:8000/test?x=8', {
+        method: 'GET',
+        // mode: 'no-cors',
+        headers: {
+            'Access-Control-Allow-Origin': 'http://localhost:8000/',
+        },
+    });
+    console.log(res)
+    
+    const body = await res.json()
+    console.log(body)
+  }
+  // TestAPI1()
   return (
     <main>
       <h1>hello nextjs</h1>
