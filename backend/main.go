143a15473a5eddd4ab02cd9da8bb6009dfd9312e
TO053037 <67772806+TO053037@users.noreply.github.com>
2023-01-18 14:02:41 +0900

    add: create test_db1

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
 	// "time"
 
 	"github.com/gin-gonic/gin"
 	// "github.com/gin-contrib/cors"
+
+	"backend/models"
 )
 
 func main() {
 	r := gin.Default()
-
+	fmt.Println(models.DbConnection)
 	r.GET("/test/:x", func(c *gin.Context) {
 		x := c.Param("x")
-		c.IndentedJSON(http.StatusOK, "Hello Golang" + x)
+		c.IndentedJSON(http.StatusOK, "Hello Golang"+x)
 	})
 	r.Run(":8000")
 }
