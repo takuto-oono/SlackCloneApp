9e85eeee89f89a7631d8f28997cae15eb0274799
TO053037 <67772806+TO053037@users.noreply.github.com>
2023-01-19 10:24:01 +0900

    add: backend user api

---
+++ backend/handler/user.go
@@ -0,0 +1,96 @@
+package handler
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+
+	"backend/models"
+)
+
+type userInput struct {
+	Name     string `json:"name"`
+	PassWord string `json:"password"`
+}
+
+func GetUsers(c *gin.Context) {
+	users, err := models.GetAllUsers()
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, users)
+}
+
+func GetUser(c *gin.Context) {
+	id := c.Param("id")
+	user, err := models.GetUserById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, user)
+}
+
+func PostUser(c *gin.Context) {
+	// test curl cmd
+	// curl -X POST -H "Content-Type: application/json" -d '{"name": "abc", "password":"pass"}' http://localhost:8000/user
+
+	var input userInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error message": err})
+		return
+	}
+	uuidWithHyphen := uuid.New()
+	uuid := strings.Replace(uuidWithHyphen.String(), "-", "", -1)
+	user := models.NewUser(uuid, input.Name, input.PassWord)
+	err := user.Create()
+	if err == nil {
+		c.IndentedJSON(http.StatusOK, input)
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error_message": err})
+	}
+}
+
+func UpdateUser(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error message": "not found id"})
+		return
+	}
+
+	var input userInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error message": "not found user information"})
+		return
+	}
+	user := models.NewUser(id, input.Name, input.PassWord)
+
+	if err := user.UpdateUser(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, user)
+}
+
+func DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error message": "not found id"})
+		return
+	}
+	var input userInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error message": "not found user information"})
+		return
+	}
+	user := models.NewUser(id, input.Name, input.PassWord)
+	if err := user.DeleteUser(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, user)
+}
--- backend/main.go
+++ backend/main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
-
 	// "time"
 
 	"github.com/gin-gonic/gin"
 	// "github.com/gin-contrib/cors"
 
+	"backend/handler"
 	"backend/models"
 )
 
 func main() {
 	r := gin.Default()
 	fmt.Println(models.DbConnection)
-	models.CreateTestData()
-	fmt.Println("---------------------------------------")
-	models.GetTestData(2)
+
+	// user handler (api test 2)
+	r.GET("/users", handler.GetUsers)
+	r.GET("/user/:id", handler.GetUser)
+	r.POST("/user", handler.PostUser)
+	r.PATCH("/user/:id", handler.UpdateUser)
+	r.DELETE("/user/:id", handler.DeleteUser)
+
+	// test api 1 handler
 	r.GET("/test/:x", func(c *gin.Context) {
 		x := c.Param("x")
 		c.IndentedJSON(http.StatusOK, "Hello Golang"+x)
