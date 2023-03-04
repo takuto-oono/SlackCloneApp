31abee058eace2779310e4d9c300cf22cfa052b0
attyoh <104344314+attyoh@users.noreply.github.com>
2023-03-04 17:01:22 +0900

    ホットリロード設定

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  // ホットリロード設定
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 800,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+  // ここまで
 }
 
 module.exports = nextConfig
