d469764942e0961511cae6827f7c0e876302f4f9
attyoh <104344314+attyoh@users.noreply.github.com>
2023-03-05 14:37:59 +0900

    ホットリロード設定をnext.config.js からdocker-compose.ymlに変更 Fixes #83

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
-  // ホットリロード設定
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 800,
-      aggregateTimeout: 300,
-    }
-    return config
-  }
-  // ここまで
 }
 
 module.exports = nextConfig
+
