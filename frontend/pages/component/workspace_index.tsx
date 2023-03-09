0e9d2941ff8cb464b384b7c083341f9a2e9de2d5
natsumoko <65808164+Matsumoto-mm12@users.noreply.github.com>
2023-03-10 08:54:58 +0900

    feat: show_workspace/[id]への移動

--- frontend/pages/component/workspace_index.tsx
+++ frontend/pages/component/workspace_index.tsx
@@ -3,33 +3,32 @@ import { getWorkspaces, Workspace } from 'pages/fetchAPI/workspace'
 import { getToken } from "@/pages/fetchAPI/cookie";
 import Link from 'next/link'
 // import { Link } from "react-router-dom";
-const query = {
-  id: 1,
-  name: "yakkun",
-}
+
 
 function WorkspaceIndex() {
   const [workspaceList, setWorkspaceList] = useState<Workspace[]>([]);
 
   const list = workspaceList.map((item, index) => (
     <div key={index}>
-      <Link href={"/component/show_workspace/"+item.id}>
+      <Link href={{ pathname: "/component/show_workspace/"+item.id, query: item.id.toString}} as={"/component/show_workspace/"+item.id}>
           Workspace{item.id} &gt;&gt;
       </Link><br></br>
-      <p>{item.id}</p>
-      <p>{item.name}</p>
-      <p>{item.primary_owner_id}</p>
+      <p>id:{item.id}</p>
+      <p>owner_id:{item.primary_owner_id}</p>
+      <p>---</p>
     </div>
   ));
   const handleGetWorkspaces = () => {
-    if (typeof getToken() !== 'undefined') {
+    // if (typeof getToken() !== 'undefined') {
       getWorkspaces().then((workspaces: Workspace[]) => {
         console.log("workspaces")
         console.log(workspaces)
-        setWorkspaceList(workspaces)
-        console.log(workspaceList)
+        if (Array.isArray(workspaces)) {
+          setWorkspaceList(workspaces)
+        }
+          console.log(workspaceList)
       });
-    }
+    // }
   }
 
   return (
