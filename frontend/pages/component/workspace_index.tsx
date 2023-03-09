164d5f2e50b6a548accff790ab823835b8956925
natsumoko <65808164+Matsumoto-mm12@users.noreply.github.com>
2023-03-09 22:11:55 +0900

    fix: workspaceListにセットする値が配列かを確認する

--- frontend/pages/component/workspace_index.tsx
+++ frontend/pages/component/workspace_index.tsx
@@ -1,33 +1,35 @@
 import React, { useState } from "react";
-import { getWorkspaces, Workspace } from 'pages/fetchAPI/workspace'
+import { getWorkspaces, Workspace } from 'pages/fetchAPI/workspace';
 
 function WorkspaceIndex() {
   const [workspaceList, setWorkspaceList] = useState<Workspace[]>([]);
-
-  const list = Object.values(workspaceList).map((item, index) => (
+  const list = workspaceList.map((item, index) => (
     <div key={index}>
       <p>{item.id}</p>
       <p>{item.name}</p>
       <p>{item.primary_owner_id}</p>
     </div>
   ));
+  
   const handleGetWorkspaces = () => {
-    getWorkspaces().then((workspaces: Workspace[]) => {
-      console.log("workspaces")
-      console.log(workspaces)
-      setWorkspaceList(workspaces)
-      console.log(workspaceList)
-    });
+        getWorkspaces().then((workspaces: Workspace[]) => {
+        console.log("workspaces")
+          console.log(workspaces)
+          if (Array.isArray(workspaces)) {
+            setWorkspaceList(workspaces)
+          }
+        console.log(workspaceList)
+      });
   }
-
-  return (
-    <div className="App">
-      <button onClick={handleGetWorkspaces}>ワークスペース一覧を表示</button>
-      <div>
-        {list}
+  
+    return (
+      <div className="App">
+        <button onClick={handleGetWorkspaces}>ワークスペース一覧を表示</button>
+        <div>
+          {list}
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default WorkspaceIndex;
