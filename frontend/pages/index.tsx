06351bdbda05d83cdc06d01b1fefaddb872a7aea
natsumoko <65808164+Matsumoto-mm12@users.noreply.github.com>
2023-03-10 08:29:53 +0900

    refactor: CookiesProviderタグの移動、不要なコード削除

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,8 @@
-// import TestAPI1 from "./api/test_api_1";
-// import { User, getUsers, getUserById, postUser, updateUser, deleteUser, } from "./fetchAPI/user";
 import React from "react";
 import LoginForm from "./component/login_form";
-import { CookiesProvider } from "react-cookie";
 import Link from 'next/link'
-
-// import testAPI1 from "./api/test_api_1";
 export default function Home() {
   return (
-    <CookiesProvider>
       <main>
         <h1>hello nextjs</h1>
         <h2>login</h2>
@@ -17,7 +11,6 @@ export default function Home() {
           Go to WorkspaceIndex &gt;&gt;
         </Link>
       </main>
-    </CookiesProvider>
   )
 }
 
