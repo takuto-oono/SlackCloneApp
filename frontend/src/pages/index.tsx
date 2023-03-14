0fb3a8e21baa60f0a524149b01b6580355f5895c
natsumoko <65808164+Matsumoto-mm12@users.noreply.github.com>
2023-03-15 06:00:23 +0900

    format: 不要なコード削除

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import LoginForm from "src/components/login_form";
-import Link from 'next/link'
 export default function Home() {
   return (
       <main>
         <h2>Login</h2>
         < LoginForm />
-        <p>---</p>
-        <Link href="workspace_index">
-          WorkspaceIndex &gt;&gt;
-        </Link>
       </main>
   )
 }
