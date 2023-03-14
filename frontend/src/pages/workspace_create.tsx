6423fe50519c1465a1232750538bb8a9bb44ac42
natsumoko <65808164+Matsumoto-mm12@users.noreply.github.com>
2023-03-15 06:04:41 +0900

    fix: 誤字修正

--- frontend/src/pages/workspace_create.tsx
+++ frontend/src/pages/workspace_create.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CreateWorkspace from "src/components/workspace/create_workspace";
-export default function CreatW() {
+export default function CreateW() {
   return (
       <main>
         < CreateWorkspace />
