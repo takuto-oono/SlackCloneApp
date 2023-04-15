69aa0dee7f0171c83d9f7b6d74e55402de810d07
natsumoko <65808164+Matsumoto-mm12@users.noreply.github.com>
2023-04-16 08:34:01 +0900

    不要なファイル削除

--- frontend/src/pages/workspace/create.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import CreateWorkspace from "src/components/workspace/create_workspace";
-export default function CreateW() {
-  return (
-      <main>
-        < CreateWorkspace />
-      </main>
-  )
-}
--- frontend/src/pages/workspace/show/[id].tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ShowWorkspace from "src/components/workspace/show_workspace/[id]";
-export default function ShowW() {
-  return (
-      <main>
-        < ShowWorkspace />
-      </main>
-  )
-}
