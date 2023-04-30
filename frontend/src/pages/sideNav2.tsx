bd33a49f5e93e8dcca89ecfdfff0319e42505c17
natsumoko <65808164+Matsumoto-mm12@users.noreply.github.com>
2023-04-30 22:21:57 +0900

    chore: SideNav2構造修正

--- frontend/src/pages/sideNav2.tsx
+++ frontend/src/pages/sideNav2.tsx
@@ -6,28 +6,30 @@ import { Outlet } from "react-router-dom";
 
 export default function SideNav2() {
   return (
-    <div style={{ display: 'flex', height: '100%', backgroundColor: "rgb(63, 14, 64)" }}>
-      <div className={classes.item}>
-            <ProSidebarProvider>
-              <Sidebar>
-                <Menu
-                  menuItemStyles={{
-                    button: () => {
-                        return {
-                          color: 'rgb(153, 133, 156)',
-                          backgroundColor: 'rgb(63, 14, 64)' 
-                        };
-                    },
-                  }}
-                >
-                  < ChannelIndex />
-                </Menu>
-              </Sidebar>
-            </ProSidebarProvider>
-          </div>
-          <div className={classes.item}>
-            <Outlet />
-          </div>
+    <div className={classes.container}>
+      <div style={{ display: 'flex', height: '100%', backgroundColor: "rgb(63, 14, 64)" }}>
+        <div className={classes.item}>
+          <ProSidebarProvider>
+            <Sidebar>
+              <Menu
+                menuItemStyles={{
+                  button: () => {
+                      return {
+                        color: 'rgb(153, 133, 156)',
+                        backgroundColor: 'rgb(63, 14, 64)' 
+                      };
+                  },
+                }}
+              >
+                < ChannelIndex />
+              </Menu>
+            </Sidebar>
+          </ProSidebarProvider>
+        </div>
+      </div>
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 }
