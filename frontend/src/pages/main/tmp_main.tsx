357b3b6b654962c55bf170182232560ab2c611c9
natsumoko <65808164+Matsumoto-mm12@users.noreply.github.com>
2023-04-30 22:24:22 +0900

    chore: 背景色変更

--- frontend/src/pages/main/tmp_main.tsx
+++ frontend/src/pages/main/tmp_main.tsx
@@ -3,7 +3,7 @@ import classes from '@styles/Home.module.css'
 function TmpMain() {
   
   return (
-    <div style={{ display: 'flex', height: '100%', width: '100vw', backgroundColor: "white" }}>
+    <div style={{ display: 'flex', height: '100%', width: '100vw'}}>
       <div className={classes.item}>
         <p>main</p>
       </div>
