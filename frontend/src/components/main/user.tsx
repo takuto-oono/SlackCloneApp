37ae94e5ce57f12163f78e60fa51351a60fc011b
natsumoko <65808164+Matsumoto-mm12@users.noreply.github.com>
2023-05-15 01:37:01 +0900

    add: form validation

--- frontend/src/components/main/user.tsx
+++ frontend/src/components/main/user.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useCookies } from "react-cookie";
 import { currentUser, login } from '@fetchAPI/login'
 import { resetCookie } from "@src/fetchAPI/cookie";
 import router from "next/router";
 import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
 
 
 
@@ -12,13 +14,13 @@ const LoginForm = () => {
   const [password, setPassword] = useState("");
   const [cookies, setCookie, removeCookie] = useCookies(['token','user_id']);
 
-  const nameChange = (e: any) => {
+  const nameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
   };
-  const passwordChange = (e: any) => {
+  const passwordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
-  const handleLogin = () => {
+  const handleSubmit = () => {
     console.log("login");
     let user = { name: name, password: password }
     login(user).then((currentUser: currentUser) => { 
@@ -32,13 +34,19 @@ const LoginForm = () => {
 
   return (
     <div className="App">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
         <label>名前
-          <input type="text" value={ name } name="name" onChange={(e) => nameChange(e)} />
+          <input type="text" value={ name } name="name" onChange={(e) => nameChange(e)} maxLength={80} required />
         </label><br />
         <label>パスワード
-          <input type="password" value={ password } name="password" onChange={(e) => passwordChange(e)} />
+          <input type="password" value={ password } name="password" onChange={(e) => passwordChange(e)} minLength={6} maxLength={72} required />
         </label><br />
-        <button onClick={handleLogin} >ログイン</button>
+        <input type="submit" value="ログイン" />
+      </form>
+      <Link to="/signUp_form">
+        <button>まだアカウントを持っていませんか？</button>
+      </Link>
     </div>
   );
 }
--- frontend/src/pages/main/login_form.tsx
+++ frontend/src/pages/main/login_form.tsx
@@ -1,27 +1,21 @@
 import React, { useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { LoginForm } from "@src/components/main/user";
-import { Link,useNavigate } from 'react-router-dom';
-import router from "next/router";
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [cookies, setCookie, removeCookie] = useCookies(['token', 'user_id']);
   const navigate = useNavigate();
   useEffect(() => {
     if (cookies.token) {
-      // console.log(cookies.token)
       navigate("workspace");
-      
     }
   },[cookies.token])
-  return <div>
-    <h2>Login</h2>
-    < LoginForm />
-    <br></br>
-    <Link to="/signUp_form">
-      <button>まだアカウントを持っていませんか？</button>
-    </Link>
-  </div>;
+  return (
+    <div>
+      < LoginForm />
+    </div>
+  );
 }
 
 export default Login;
