689c993cb087eb70785581ddc5b94ad2eaa48272
TO053037 <67772806+TO053037@users.noreply.github.com>
2023-05-31 22:46:07 +0900

    fix: break-wordからbreak-allに変更

--- frontend/src/components/main/show_messages.tsx
+++ frontend/src/components/main/show_messages.tsx
@@ -24,7 +24,7 @@ const showItem: React.FC<Message> = (
 		<>
 			<p>{userName}</p>
 			<p>{message.createdAt}</p>
-			<p className="break-words">{message.text}</p>
+			<p className="break-all">{message.text}</p>
 		</>
 	);
 };
